c6ebdbd53bae165119adaa035d9e1b53270d9172
Joshua Udeh <udehasilo@gmail.com>
2021-03-01 12:46:22 +0100

    exercise 2

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -5,7 +5,7 @@
   // check if the current is even or odd
 
   // report the result to the screen
-
+/*
   for (var step = 0; step < 21; step++) {
     if(step % 2===0) {
       console.log(step + " is even");
@@ -14,4 +14,23 @@
         console.log(step + " is odd");
       }
     }
-  }
+  }*/
+
+  //while loop
+/*
+  var step = 0;
+  while (step < 21) {
+    if (step %2===0) 
+    console.log(step + " is even");
+    else 
+    console.log(step + " is odd");
+    step++
+  }*/
+
+console.log("Do while loop")
+  var step = 0;
+do{
+  if(step % 2 ==0) console.log(step+" is even");
+  else console.log(step+" is odd");
+  step++;
+}while(step <= 20);
